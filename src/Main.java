f415fc69f1ffb266b0468cac6360db223d6d716d
sosuisen <hidekazu.kubota@gmail.com>
2023-01-14 15:53:01 +0900

    feat: add benchmark

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Printable p = new PrinterProxy("Alice");
+		long start = System.currentTimeMillis();
+        // Printable p = new PrinterProxy("Alice");
+		Printable p = new Printer("Alice");
         System.out.println("名前は現在" + p.getPrinterName() + "です。");
         p.setPrinterName("Bob");
         System.out.println("名前は現在" + p.getPrinterName() + "です。");
+		long lap1 = System.currentTimeMillis();
+		System.out.println("ここまでの所要時間：" + (lap1 - start) + "ms");
         p.print("Hello, world.");
+		long lap2 = System.currentTimeMillis();
+		System.out.println("ここまでの所要時間：" + (lap2 - start) + "ms");
     }
 }
